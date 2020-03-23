4fe9000973c4ea9e8c196753cf784ac128e440b5
HironoriSato <maza.hs5360@gmail.com>
2020-03-23 23:48:04 +0900

    [add]userのemailの@以下を小文字にするようメソッドを使用, テストの作成

--- app/core/models.py
+++ app/core/models.py
@@ -7,7 +7,7 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
         # ユーザーのフィールドを設定(**extra_fieldsで追加するフィールドをセット)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         # パスワードは暗号化してセット
         user.set_password(password)
         user.save(using=self._db)
