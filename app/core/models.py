1ee85cc2c8dcaad0002c30556e3e363052cbef8e
HironoriSato <maza.hs5360@gmail.com>
2020-03-23 23:41:42 +0900

    [add]AbstractBaseUserを使用してカスタムユーザーのModelを作成, UserManagerを作成, テストケースの作成

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves a new user"""
+        # ユーザーのフィールドを設定(**extra_fieldsで追加するフィールドをセット)
+        user = self.model(email=email, **extra_fields)
+        # パスワードは暗号化してセット
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    # usernameの代わりにemailを使用
+    USERNAME_FIELD = 'email'
