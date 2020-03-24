5bbf34cd07dc35d26144815abd55607441b92343
HironoriSato <maza.hs5360@gmail.com>
2020-03-24 21:59:18 +0900

    [add]userAdmin画面のフォームをCustomUserに合わせてカスタマイズ

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
+# 多言語に翻訳するためのメソッド
 
 from . import models
 
@@ -7,6 +9,16 @@ from . import models
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
+    # userAdmin画面のフォームの設定
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name', )}),
+        (
+            _('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (_('Important dates'), {'fields': ('last_login',)})
+    )
 
 
 admin.site.register(models.User, UserAdmin)
