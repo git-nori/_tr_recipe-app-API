1d58005457c900986dc192dc92e629ad8cf79de0
HironoriSato <maza.hs5360@gmail.com>
2020-04-03 23:52:31 +0900

    [add]ユーザー更新用APIを作成

--- app/user/views.py
+++ app/user/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from .serialize import UserSerializer, AuthTokenSerializer
 from rest_framework.settings import api_settings
@@ -14,3 +14,16 @@ class CreateTokenView(ObtainAuthToken):
     serializer_class = AuthTokenSerializer
     # settings.pyのDEFAULT_RENDERER_CLASSESをセット
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+    authentication_classes = (authentication.TokenAuthentication,)
+
+    # get_queryset => 複数件取得時など
+    # get_object => １件取得時など
+    def get_object(self):
+        """Retrieve and return authentication user"""
+        return self.request.user
